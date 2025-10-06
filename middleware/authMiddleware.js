c2fc353ced76f90648f7598ecca98938b994ec46
jefrin07 <jefrin.ferolous08@gmail.com>
2025-10-06 19:47:53 +0530

    thrrd

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,33 +2,31 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protect = async (req, res, next) => {
-  let token;
+  let token = null;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
-
-      // Attach user to request object
-      req.user = await User.findById(decoded.id).select("-password");
-
-      if (!req.user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-
-      next();
-    } catch (err) {
-      console.error(err);
-      return res.status(401).json({ message: "Not authorized, token failed" });
-    }
+    token = req.headers.authorization.split(" ")[1];
   }
 
   if (!token) {
+    console.log("Token received:", token);
     return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
+    req.user = await User.findById(decoded.id).select("-password");
+
+    if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({ message: "Not authorized, token failed" });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const authRoutes = express.Router();
 
 authRoutes.post("/login", loginUser);
-authRoutes.post("/me", me);
+authRoutes.post("/me",protect, me);
 authRoutes.get("/getUserProfile",protect, getUserProfile);
 authRoutes.put("/updateUserProfile",protect, updateUserProfile);
 
